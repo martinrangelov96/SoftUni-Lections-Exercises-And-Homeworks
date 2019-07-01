c2774a2ae4ce54db2a4ba0c7f23e7ac08affac2c
martinrangelov96 <martin.rangelov96@gmail.com>
2019-07-01 09:51:03 +0300

    Initial Commit

---
+++ Java MVC Frameworks - Spring SoftUni/08_Workshop_ProductShop/src/test/java/org/softuni/productshop/ProductshopApplicationTests.java
@@ -0,0 +1,16 @@
+package org.softuni.productshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductshopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
